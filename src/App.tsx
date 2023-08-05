32d166d3679c1cec51423e75ccb44e272513bbbb
Maria Baeta <m.martinsdg@gmail.com>
2023-08-04 22:16:11 -0300

    style: :lipstick: Configurando Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+
+import { Col, Container, Row } from 'react-bootstrap'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <p>Hello
-
-      </p>
-    </>
+    <Container fluid>
+      <Row className="cabecalho">
+        <Col>Cabeçalho</Col>
+      </Row>
+      <Row className='body'>
+        <Col md={2} className='navBar'>Navbar</Col>
+        <Col md={8} className='bg-sucess'>Body</Col>
+      </Row>
+    </Container>
   )
 
 }
